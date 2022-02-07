a89d1838f1b26e5451548148bef28e3cee0b7306
Katherine Piniol <piniolk@hawaii.edu>
2022-02-07 12:24:53 -1000

    test set up

---
+++ my-app/src/assets/TopMenu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TopMenu extends React.Component {
+  render() {
+    return (
+        <div className="App">
+              <header className="App-header">
+                <p>
+                  test index page
+                </p>
+              </header>
+            </div>
+    )
+  }
+}
+
+export default TopMenu;
