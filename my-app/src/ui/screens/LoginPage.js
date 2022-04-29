60d85c1f05fad67d79aed23b35fd2cd0fb7b4f80
Katherine Piniol <piniolk@hawaii.edu>
2022-04-29 12:08:31 -1000

    Login Page from Darnell's branch

--- my-app/src/ui/screens/LoginPage.js
+++ my-app/src/ui/screens/LoginPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import '../../App.css';
 import TopMenu from '../components/TopMenu'
 
@@ -5,7 +6,27 @@ function LoginPage() {
   return (
   <div className='App'>
     <TopMenu />
-    <a>Login page</a>
+     <div className="login-container">
+    <h1 className="login-title">Login</h1>
+    <div className="login-username">
+      <p>
+        <h3>Username</h3>
+      </p>
+      <p>
+        <input placeholder="Username..."/>
+      </p>
+    </div>
+    <div className="login-password">
+      <p>
+        <h3>Password</h3>
+      </p>
+      <p>
+        <input placeholder="Password..."/>
+      </p>
+    </div>
+    <div className="login-button" >Login</div>
+    <div className="login-new-account">Need an account? <a href="">Click here</a></div>
+  </div>
   </div>
   );
 }
--- my-app/src/ui/screens/PlannerPage.js
+++ my-app/src/ui/screens/PlannerPage.js
@@ -8,11 +8,11 @@ function PlannerPage() {
   return (
   <div className='App'>
     <TopMenu />
-  <div className="budgetform-container">
-    <h1 className="budgetform-title">Planner</h1>
-    <Link className={"TopMenu-button"} to="/viewDates">VIEW OLD SHEETS</Link>
-    <Link className={"TopMenu-button"} to="/budgetForm">CREATE NEW SHEET</Link>
-</div>
+    <div className="budgetform-container">
+      <h1 className="budgetform-title">Planner</h1>
+      <Link className={"TopMenu-button"} to="/viewDates">VIEW OLD SHEETS</Link>
+      <Link className={"TopMenu-button"} to="/budgetForm">CREATE NEW SHEET</Link>
+    </div>
   </div>
   );
 }
